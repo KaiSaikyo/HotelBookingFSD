4bc985eb86faa862395699993e0dafdb1e452f0e
Kai <KaiPsycho100@gmail.com>
2024-01-22 00:14:03 +0800

    added back for all pages

--- HotelBooking/Shared/Domain/Staff.cs
+++ HotelBooking/Shared/Domain/Staff.cs
@@ -3,19 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace HotelBooking.Shared.Domain
 {
-    public class Staff
+    public class Staff : IValidatableObject
     {
         public int Id { get; set; }
-
-        public string? Name { get; set; }
+		[Required]
+		public string? Name { get; set; }
 
         public string? Email { get; set; }
 
         public string? Password { get; set;}
 
         public string? Mobile { get; set; }
-    }
+
+		public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+		{
+			throw new NotImplementedException();
+		}
+	}
 }
